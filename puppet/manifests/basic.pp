3ff6a2b1a3315c00937398b1c47c55dd7e443d91
Brandon Casey(Work) <iN7h33nD@gmail.com>
2014-03-25 17:11:15 -0400

    Install some basic system things

---
+++ puppet/manifests/basic.pp
@@ -0,0 +1,35 @@
+class basic{
+  # run apt-get update before anything else runs
+  class {"basic::users":} ->
+  class {"basic::packages":} ->
+  class {"basic::helpers":}
+}
+
+class basic::users{
+  group { "puppet":
+    ensure => "present",
+  }
+}
+
+# just some packages
+class basic::packages{
+  package{"curl": ensure => installed}
+  package{"vim":  ensure => installed}
+}
+
+
+class basic::helpers{
+  $puppet_dir = "/vagrant/puppet"
+  # script to run puppet
+  file{"/usr/local/bin/runpuppet":
+    content => " \
+    sudo puppet apply -vv  --modulepath=$puppet_dir/modules/ $puppet_dir/manifests/main.pp\n",
+    mode    => 0755
+  }
+
+  # script to run librarian-puppet
+  file{"/usr/local/bin/runlibrarian":
+    content => "cd $puppet_dir &&  sudo librarian-puppet update \n",
+    mode    => 0755
+  }
+}
